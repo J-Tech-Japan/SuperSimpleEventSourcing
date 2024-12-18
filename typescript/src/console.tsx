0a659209cae9e82653553e517bd73df21b624286
Tomohisa Takaoka <ttakaoka@jtechs.com>
2024-12-17 22:26:01 -0800

    Typescript Aggregate

--- typescript/src/console.tsx
+++ typescript/src/console.tsx
@@ -1,14 +1,30 @@
 // hello world
 console.log('hello world');
 
-import { BranchCreated } from './domain';
+import { BranchCreated, BranchProjector } from './domain';
+import { PartitionKeys, EventCommon, EmptyAggregatePayload } from './lib';
+import { v4 as uuidv4 } from 'uuid';
 
-// const branch: BranchCreated = {
-//     type: 'BranchCreated',
-//     name: 'London',
-//     country: 'UK',
-//     IsEventPayload: () => true,
-// };
+const branchCreatedEventPaylaod = new BranchCreated('London', 'UK');
+console.log(branchCreatedEventPaylaod);
 
-const branch = new BranchCreated('London', 'UK');
+const partitionKey : PartitionKeys = {
+    AggregateID: uuidv4(),
+    Group: 'Branch',
+    RootPartitionKey: 'Branch',
+};
+console.log(partitionKey);
+
+const branchCreatedEvent : EventCommon = {
+    Version: 1,
+    SortableUniqueID: '1',
+    PartitionKeys: partitionKey,
+    Payload: branchCreatedEventPaylaod,
+};
+console.log(branchCreatedEvent);
+
+const branchProjector = new BranchProjector();
+console.log(branchProjector.GetVersion());
+const branch = branchProjector.Project(new EmptyAggregatePayload(), branchCreatedEvent);
+
 console.log(branch);
--- typescript/src/lib.ts
+++ typescript/src/lib.ts
@@ -1,7 +1,60 @@
-// type EventPayloadBase = {
-//     IsEventPayload: () => boolean;
-// };
-// type EventPayload<T> = T & EventPayloadBase;
-interface EventPayload {
+export interface EventPayload {
     IsEventPayload() : boolean;
+}
+
+export type PartitionKeys = {
+    AggregateID: string;  // UUID is represented as a string in TypeScript
+    Group: string;
+    RootPartitionKey: string;
+}
+
+export type EventCommon = {
+    Version: number;
+    SortableUniqueID: string;
+    PartitionKeys: PartitionKeys;
+    Payload: EventPayload;
+}
+
+export interface AggregatePayload {
+    IsAggregatePayload(): boolean;
+}
+
+export class EmptyAggregatePayload implements AggregatePayload {
+    IsAggregatePayload(): boolean {
+        return true;
+    }
+}
+
+export class Aggregate {
+    Payload: AggregatePayload;
+    PartitionKeys: PartitionKeys;
+    Version: number;
+    LastSortableUniqueID: string;
+
+    constructor(payload: AggregatePayload, partitionKeys: PartitionKeys, version: number, lastSortableUniqueID: string) {
+        this.Payload = payload;
+        this.PartitionKeys = partitionKeys;
+        this.Version = version;
+        this.LastSortableUniqueID = lastSortableUniqueID;
+    }
+    Project(ev: EventCommon, projector: AggregateProjector): Aggregate {
+        return new Aggregate(
+            projector.Project(this.Payload, ev),  // Payload をプロジェクタで更新
+            this.PartitionKeys,
+            ev.Version,
+            ev.SortableUniqueID
+        );
+    }
+    ProjectAll(events: EventCommon[], projector: AggregateProjector): Aggregate {
+        var updated : Aggregate = this;
+        for (const ev of events) {
+            updated = updated.Project(ev, projector);  // 各イベントで Project を呼び出して更新
+        }
+        return updated;
+    }
+}
+
+export interface AggregateProjector {
+    Project(payload: AggregatePayload, ev: EventCommon): AggregatePayload;
+    GetVersion(): string;
 }
